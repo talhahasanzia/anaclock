0f0476b60770c881b1ef05a7eab02f88f96ead67
Talha <talhahasanzia@hotmail.com>
2018-09-21 16:17:23 +0500

    Base skeleton ready

--- anaclocklib/src/main/java/com/thz/anaclocklib/AnaClock.java
+++ anaclocklib/src/main/java/com/thz/anaclocklib/AnaClock.java
@@ -1,20 +1,16 @@
 package com.thz.anaclocklib;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.Calendar;
@@ -22,26 +18,88 @@ import java.util.Calendar;
 
 public class AnaClock extends View {
 
-    private static final String TAG = "ANACLOCK";
-    private final static int SMALL_HAND = 1;
-    private final static int BIG_HAND = 2;
-    private int hour, minutes;
-    private double smallHandStartX, smallHandEndX, bigHandStartX, bigHandEndX, smallHandStartY, smallHandEndY, bigHandStartY, bigHandEndY;
-    private float smallHandLength, bigHandLength, dialRadius;
-    private float smallHandWidth, bigHandWidth, dialWidth;
-    private float validPadding;
-    private double smallHandSlope, bigHandSlope;
-    private int smallHandColor, bigHandColor;
-    private boolean isDrawingAllowed = true;
-    private boolean isActive;
-    private final BroadcastReceiver tickReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (isActive)
-                updateTime();
-        }
-    };
-    private int dialColor;
+
+    ///--------- dial values ---------///
+    protected float dialRadius;
+
+    protected float dialWidth;
+
+    protected int dialColor;
+
+    @DrawableRes
+    protected int dialResource;
+
+    ///--------- hours hand values ---------///
+    protected final static int HOURS_HAND = 1;
+
+    protected int hour;
+
+    protected double hoursHandEndX, hoursHandEndY;
+
+    protected float hoursHandLength;
+
+    protected double hoursHandSlope;
+
+    protected float hoursHandWidth;
+
+    protected int hoursHandColor;
+
+    @DrawableRes
+    protected int hoursHandResource;
+
+
+    ///--------- minutes hand values ---------///
+    protected final static int MINUTES_HAND = 2;
+
+    protected int minutes;
+
+    protected double minutesHandEndX, minutesHandEndY;
+
+    protected float minutesHandLength;
+
+    protected float minutesHandWidth;
+
+    protected double minutesHandSlope;
+
+    protected int minutesHandColour;
+
+    @DrawableRes
+    protected int minutesHandResource;
+
+    ///--------- seconds hand values ---------///
+    protected final static int SECONDS_HAND = 3;
+
+
+    protected int seconds;
+
+    protected double secondsHandEndX, secondsHandEndY;
+
+    protected float secondsHandLength;
+
+    protected float secondsHandWidth;
+
+    protected double secondsHandSlope;
+
+    protected int secondsHandColour;
+
+    protected final long DELAY_SECOND = 1000;
+
+    @DrawableRes
+    protected int secondsHandResource;
+
+
+    ///--------- global variables ---------///
+    protected float validPadding;
+
+    protected boolean isDrawingAllowed = true;
+
+    protected boolean isActive;
+
+    protected Runnable runnable;
+
+    protected Handler timeHandler;
+
+    Paint paint;
 
     public AnaClock(Context context) {
         super(context);
@@ -64,7 +122,7 @@ public class AnaClock extends View {
         init(context, attrs);
     }
 
-    private void init(Context context, AttributeSet attributeSet) {
+    protected void init(Context context, AttributeSet attributeSet) {
         if (attributeSet != null) {
 
             TypedArray attributeArray = context.obtainStyledAttributes(attributeSet, R.styleable.AnaClock);
@@ -72,14 +130,17 @@ public class AnaClock extends View {
 
             hour = attributeArray.getInteger(R.styleable.AnaClock_start_hour, Calendar.getInstance().get(Calendar.HOUR));
             minutes = attributeArray.getInteger(R.styleable.AnaClock_start_minutes, Calendar.getInstance().get(Calendar.MINUTE));
+            seconds = attributeArray.getInteger(R.styleable.AnaClock_start_seconds, Calendar.getInstance().get(Calendar.SECOND));
             dialColor = attributeArray.getColor(R.styleable.AnaClock_dial_color, Color.BLUE);
-            smallHandColor = attributeArray.getColor(R.styleable.AnaClock_small_hand_color, Color.RED);
-            bigHandColor = attributeArray.getColor(R.styleable.AnaClock_big_hand_color, Color.BLUE);
+            hoursHandColor = attributeArray.getColor(R.styleable.AnaClock_hours_hand_color, Color.RED);
+            minutesHandColour = attributeArray.getColor(R.styleable.AnaClock_minutes_hand_color, Color.BLUE);
+            secondsHandColour = attributeArray.getColor(R.styleable.AnaClock_seconds_hand_color, Color.CYAN);
             isActive = attributeArray.getBoolean(R.styleable.AnaClock_is_active, true);
             attributeArray.recycle();
 
         }
-
+        // init here to avoid init in onDraw, call paint.reset() to get a fresh paint object
+        paint = new Paint();
     }
 
     @Override
@@ -92,6 +153,7 @@ public class AnaClock extends View {
         // ensure that width and height is same
         int measuredHeight = 0;
         int measuredWidth = 0;
+
         try {
             measuredWidth = measureWidth(widthMeasureSpec);
             measuredHeight = measureHeight(heightMeasureSpec);
@@ -113,27 +175,7 @@ public class AnaClock extends View {
         setMeasuredDimension(measuredWidth, measuredHeight);
     }
 
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-
-
-        if (isAttachedToWindow()) {
-
-
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(Intent.ACTION_TIME_TICK);
-            filter.addAction(Intent.ACTION_TIME_CHANGED);
-            filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-
-            getContext().registerReceiver(tickReceiver, filter);
-
-        } else {
-            getContext().unregisterReceiver(tickReceiver);
-        }
-    }
-
-    private int measureHeight(int measureSpec) throws Exception {
+    protected int measureHeight(int measureSpec) throws Exception {
         // ensure that width and height is same
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
@@ -150,7 +192,7 @@ public class AnaClock extends View {
         return result;
     }
 
-    private int measureWidth(int measureSpec) throws Exception {
+    protected int measureWidth(int measureSpec) throws Exception {
 
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
@@ -168,37 +210,53 @@ public class AnaClock extends View {
     }
 
     @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+
+        if (isAttachedToWindow()) {
+
+            bindHandler();
+
+        } else {
+            unbindHandler();
+        }
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         float viewMidX = getWidth() / 2;
         float viewMidY = getHeight() / 2;
 
         if (isDrawingAllowed) {
 
-            dialRadius = getWidth() / 2.25f;
-            smallHandStartX = viewMidX;
-            smallHandStartY = viewMidY;
+            calculateDialRadius();
 
-            bigHandStartX = smallHandStartX;
-            bigHandStartY = smallHandStartY;
+            calculateSecondsHandLength();
 
+            calculateMinutesHandLength();
 
-            bigHandLength = getHeight() - (getHeight() / 1.45f);
-            smallHandLength = bigHandLength * 0.6f;
+            calculateHoursHandLength();
 
+            calculateHoursHandWidth();
 
-            smallHandWidth = getWidth() * 0.02f;
-            bigHandWidth = getWidth() * 0.01f;
-            dialWidth = smallHandWidth;
+            calculateMinutesHandWidth();
+
+            calculateSecondsHandWidth();
+
+            calculateDialWidth();
 
             drawDial(canvas, viewMidX, viewMidY);
 
-            drawBigHand(canvas, viewMidX, viewMidY);
+            drawSeconds(canvas, viewMidX, viewMidY);
 
-            drawSmallHand(canvas, viewMidX, viewMidY);
+            drawMinutes(canvas, viewMidX, viewMidY);
+
+            drawHours(canvas, viewMidX, viewMidY);
 
 
         } else {
-            Paint paint = new Paint();
+
             paint.setStyle(Paint.Style.FILL);
             paint.setColor(Color.RED);
             paint.setTextSize(getHeight() * 0.1f);
@@ -206,55 +264,191 @@ public class AnaClock extends View {
         }
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-                if(checkTouchedElement(event.getX(), event.getY())==SMALL_HAND)
-                    Log.d(TAG, "onTouchEvent: small hand touched");
-                else if(checkTouchedElement(event.getX(), event.getY())==BIG_HAND)
-                    Log.d(TAG, "onTouchEvent: big hand touched");
-                else
-                    Log.d(TAG, "onTouchEvent: No hand touch");
+    protected void calculateDialWidth() {
+        dialWidth = getWidth() * 0.02f;
+    }
+
+    protected void calculateSecondsHandWidth() {
+        secondsHandWidth = getWidth() * 0.005f;
+    }
+
+    protected void calculateMinutesHandWidth() {
+        minutesHandWidth = getWidth() * 0.01f;
+    }
+
+    protected void calculateHoursHandWidth() {
+        hoursHandWidth = getWidth() * 0.02f;
+    }
+
+    protected void calculateHoursHandLength() {
+        hoursHandLength = (getHeight() - (getHeight() / 1.45f)) * 0.6f;
+    }
+
+    protected void calculateSecondsHandLength() {
+        secondsHandLength = getHeight() * 0.42f;
+    }
+
+    protected void calculateMinutesHandLength() {
+        minutesHandLength = getHeight() - (getHeight() / 1.45f);
+    }
+
+    protected void calculateDialRadius() {
+        dialRadius = getWidth() / 2.25f;
+    }
+
+    protected void drawSeconds(Canvas canvas, float viewMidX, float viewMidY) {
+        paint.reset();
+        paint.setStrokeWidth(secondsHandWidth);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(secondsHandColour);
 
 
+        secondsHandSlope = getSecondsAngleFromTime();
+        secondsHandEndX = (secondsHandLength * Math.cos(secondsHandSlope)) + getWidth() / 2;
+        secondsHandEndY = (secondsHandLength * Math.sin(secondsHandSlope)) + getHeight() / 2;
 
-                break;
-            }
+        canvas.drawLine(viewMidX, viewMidY, (float) secondsHandEndX, (float) secondsHandEndY, paint);
+    }
 
+
+    protected void drawMinutes(Canvas canvas, float viewMidX, float viewMidY) {
+        paint.reset();
+        paint.setStrokeWidth(minutesHandWidth);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(minutesHandColour);
+
+
+        minutesHandSlope = getMinutesAngleFromTime();
+        minutesHandEndX = (minutesHandLength * Math.cos(minutesHandSlope)) + getWidth() / 2;
+        minutesHandEndY = (minutesHandLength * Math.sin(minutesHandSlope)) + getHeight() / 2;
+
+        canvas.drawLine(viewMidX, viewMidY, (float) minutesHandEndX, (float) minutesHandEndY, paint);
+    }
+
+    protected void drawHours(Canvas canvas, float viewMidX, float viewMidY) {
+        paint.reset();
+        paint.setStrokeWidth(hoursHandWidth);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(hoursHandColor);
+
+        hoursHandSlope = getHoursAngleFromTime();
+        hoursHandEndX = (hoursHandLength * Math.cos(hoursHandSlope)) + viewMidX;
+        hoursHandEndY = (hoursHandLength * Math.sin(hoursHandSlope)) + viewMidY;
+
+        canvas.drawLine(viewMidX, viewMidY, (float) hoursHandEndX, (float) hoursHandEndY, paint);
+
+    }
+
+    protected void drawDial(Canvas canvas, float viewMidX, float viewMidY) {
+        paint.reset();
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(dialColor);
+        paint.setStrokeWidth(dialWidth);
+        canvas.drawCircle(viewMidX, viewMidY, dialRadius, paint);
+    }
+
+
+    protected double getSecondsAngleFromTime() {
+
+
+        if (seconds > 59)
+            seconds = seconds - 60;
+
+        double totalInput = (hour * 60) + minutes + seconds;
+
+
+        double degreeInAngles = totalInput * 6;
+
+        return Math.toRadians(degreeInAngles - 90);
+
+
+    }
+
+    protected double getMinutesAngleFromTime() {
+
+
+        if (hour > 12)
+            hour = hour - 12;
+
+        double totalInput = (hour * 60) + minutes;
+
+
+        double degreeInAngles = totalInput * 6;
+
+        return Math.toRadians(degreeInAngles - 90);
+
+
+    }
+
+    protected double getHoursAngleFromTime() {
+
+
+        if (hour > 12)
+            hour = hour - 12;
+
+
+        float hourDecimal = hour + (minutes / 60f);
+
+        double degreeInAngles = hourDecimal * 30;
+
+
+        return Math.toRadians(degreeInAngles - 90);
+
+
+    }
+
+    protected float getValidPadding() {
+        float[] paddings = {getPaddingLeft(), getPaddingTop(), getPaddingRight(), getPaddingBottom(), getPaddingStart(), getPaddingEnd()};
+
+        float validPadding = paddings[0];
+
+        for (int i = 1; i < paddings.length; i++) {
+            if (validPadding < paddings[i])
+                validPadding = paddings[i];
         }
-        return true;
+
+        return validPadding;
     }
 
-    private int checkTouchedElement(float x, float y) {
+    protected float normalizeValue(float val) {
+        return val < 0 ? val * (-1) : val;
+    }
 
-        Point clickedPoint = new Point((int) x, (int) y);
-        Point startPoint = new Point((int) smallHandStartX, (int) smallHandStartY);
+    protected void updateTime() {
 
-        // small hand
-        Point smallHandPoint = new Point((int) smallHandEndX, (int) smallHandEndY);
+        Calendar calendar = Calendar.getInstance();
+        hour = calendar.get(Calendar.HOUR);
+        minutes = calendar.get(Calendar.MINUTE);
+        seconds = calendar.get(Calendar.SECOND);
 
-        if (inLine(startPoint, smallHandPoint, clickedPoint))
-            return SMALL_HAND;
-
-        // big hand
-        Point bigHandPoint = new Point((int) bigHandEndX, (int) bigHandEndY);
-
-        if (inLine(startPoint, bigHandPoint, clickedPoint))
-            return BIG_HAND;
-
-        return -1;
+        invalidate();
 
     }
 
-    private boolean inLine(Point A, Point B, Point C) {
-        // if AC is horizontal
-        if (A.x == C.x) return B.x == C.x;
-        // if AC is vertical.
-        if (A.y == C.y) return B.y == C.y;
-        // match the gradients
-        return (A.x - C.x) * (A.y - C.y) == (C.x - B.x) * (C.y - B.y);
+    protected void bindHandler() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (isActive) {
+                    updateTime();
+                }
+                timeHandler.postDelayed(this, DELAY_SECOND);
+            }
+        };
+
+        timeHandler = new Handler();
+        timeHandler.postDelayed(runnable, DELAY_SECOND);
+
     }
+
+    protected void unbindHandler() {
+        timeHandler.removeCallbacks(runnable);
+        runnable = null;
+        timeHandler = null;
+    }
+
+    ///----------- getters and setters  -----------///
 
     public int getHour() {
         return hour;
@@ -274,111 +468,19 @@ public class AnaClock extends View {
         invalidate();
     }
 
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
     public boolean isActive() {
         return isActive;
     }
 
     public void setActive(boolean active) {
         isActive = active;
-    }
-
-
-    private void drawBigHand(Canvas canvas, float viewMidX, float viewMidY) {
-        Paint paint = new Paint();
-        paint.setStrokeWidth(bigHandWidth);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setColor(bigHandColor);
-
-
-        bigHandSlope = getMinutesAngleFromTime();
-        bigHandEndX = (bigHandLength * Math.cos(bigHandSlope)) + getWidth() / 2;
-        bigHandEndY = (bigHandLength * Math.sin(bigHandSlope)) + getHeight() / 2;
-
-        canvas.drawLine(viewMidX, viewMidY, (float) bigHandEndX, (float) bigHandEndY, paint);
-    }
-
-    private void drawSmallHand(Canvas canvas, float viewMidX, float viewMidY) {
-        Paint paint = new Paint();
-        paint.setStrokeWidth(smallHandWidth);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setColor(smallHandColor);
-
-        smallHandSlope = getHoursAngleFromTime();
-        smallHandEndX = (smallHandLength * Math.cos(smallHandSlope)) + viewMidX;
-        smallHandEndY = (smallHandLength * Math.sin(smallHandSlope)) + viewMidY;
-
-        canvas.drawLine(viewMidX, viewMidY, (float) smallHandEndX, (float) smallHandEndY, paint);
-
-    }
-
-    private void drawDial(Canvas canvas, float viewMidX, float viewMidY) {
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(dialColor);
-        paint.setStrokeWidth(dialWidth);
-        canvas.drawCircle(viewMidX, viewMidY, dialRadius, paint);
-    }
-
-    private double getMinutesAngleFromTime() {
-
-        Log.d(TAG, "getMinutesAngleFromTime: hour: " + hour);
-        Log.d(TAG, "getMinutesAngleFromTime: mins: " + minutes);
-
-        if (hour > 12)
-            hour = hour - 12;
-
-        double totalInput = (hour * 60) + minutes;
-
-
-        double degreeInAngles = totalInput * 6;
-
-        return Math.toRadians(degreeInAngles - 90);
-
-
-    }
-
-    private double getHoursAngleFromTime() {
-
-        Log.d("ANACLOCK", "getMinutesAngleFromTime: hour: " + hour);
-
-        if (hour > 12)
-            hour = hour - 12;
-
-
-        float hourDecimal = hour + (minutes / 60f);
-
-        double degreeInAngles = hourDecimal * 30;
-
-
-        return Math.toRadians(degreeInAngles - 90);
-
-
-    }
-
-    private float getValidPadding() {
-        float[] paddings = {getPaddingLeft(), getPaddingTop(), getPaddingRight(), getPaddingBottom(), getPaddingStart(), getPaddingEnd()};
-
-        float validPadding = paddings[0];
-
-        for (int i = 1; i < paddings.length; i++) {
-            if (validPadding < paddings[i])
-                validPadding = paddings[i];
-        }
-
-        return validPadding;
-    }
-
-    private float normalizeValue(float val) {
-        return val < 0 ? val * (-1) : val;
-    }
-
-    private void updateTime() {
-
-        Calendar calendar = Calendar.getInstance();
-        hour = calendar.get(Calendar.HOUR);
-        minutes = calendar.get(Calendar.MINUTE);
-
-        invalidate();
-
     }
 }
